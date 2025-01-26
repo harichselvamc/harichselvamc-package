cb725fdc49c1c23b2b66213b700735992b073826
harichselvam <harichselvamc@gmail.com>
2025-01-27 00:11:19 +0530

    testing

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="harichselvamc",
-    version="0.5",
+    version="0.6",
     packages=find_packages(),
     description="A simple Python package to generate Pascal's Triangle.",
     long_description="""
